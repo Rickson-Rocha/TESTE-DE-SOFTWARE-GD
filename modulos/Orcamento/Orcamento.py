329391a93246fa8c61a7460b75a5c26ac4ac0280
Rickson-Rocha <ricksonroccha21@gmail.com>
2024-03-17 18:45:45 -0300

    [feature] implemente class Orcamento

---
+++ modulos/Orcamento/Orcamento.py
@@ -0,0 +1,29 @@
+from typing import Dict, Double, List
+
+from modulos.Categoria.Categoria import Categoria
+from modulos.Despesa import Despesa
+from modulos.Despesa.Despesa import Despesa
+
+
+class Orcamento:
+
+    def __init__(self,orcamentoMensal:float) -> None:
+        self._orcamentoMensal = orcamentoMensal
+        self.despesas: List[Despesa] = []
+
+
+    
+    def definirOrcamentoMensal(novoOrcamento:float):
+        pass
+
+    def adicionarDespesa(despesa:Despesa):
+        pass
+
+    def calcularTotalDespesa(self)->float:
+        pass
+
+    def calcularProgressoOrcamento(self)->float:
+        pass
+
+    def resumoDespesasPorCategoria(despesas: List[Despesa]) -> Dict[Categoria, float]:
+      pass
