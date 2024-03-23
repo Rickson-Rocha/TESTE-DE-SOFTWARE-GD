d4d6b457f47428b305c98a4eeff4cf58492d575a
anaclrra <anaclarab826@gmail.com>
2024-03-23 19:38:17 -0300

    [feature] Implementa lógica dos métodos da Classe Orçamento com base nos testes do arquivo test_orcamento.py

--- Orcamento.py
+++ Orcamento.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 
-from Categoria import Categoria
 from Despesa import Despesa
+from Categoria import Categoria
 
 
 class Orcamento:
@@ -10,17 +10,46 @@ class Orcamento:
         self._orcamentoMensal = orcamentoMensal
         self.despesas: List[Despesa] = []
 
-    def definirOrcamentoMensal(novoOrcamento:float):
-        pass
+    def definirOrcamentoMensal(self,novoOrcamento:float):
 
-    def adicionarDespesa(despesa:Despesa):
-        pass
+        if not isinstance(novoOrcamento, (int, float)):
+            raise ValueError('O orçamento mensal deve ser numérico.')
+        elif novoOrcamento == '':
+            raise ValueError('O orçamento mensal não pode ser vazio.')
+        elif novoOrcamento < 0:
+            raise ValueError('O orçamento não pode ser um valor negativo.')
+        elif novoOrcamento == 0:
+            raise ValueError('O orçamento mensal deve ser maior que zero.')
+        self._orcamentoMensal = novoOrcamento
+        
+    def adicionarDespesa(self, despesa:Despesa):
+        
+        total_despesas = self.calcularTotalDespesa()
+        if total_despesas + despesa.getValor() > self._orcamentoMensal:
+            raise ValueError('A despesa excede o orçamento mensal.')
+        self.despesas.append(despesa)
+
+    def removerDespesa(self, despesa: Despesa):
+        self.despesas.remove(despesa)
 
     def calcularTotalDespesa(self)->float:
-        pass
+        total = sum(despesa.getValor() for despesa in self.despesas)
+        return total
 
-    def calcularProgressoOrcamento(self)->float:
-        pass
+    def calcularProgressoOrcamento(self) -> str:
 
-    def resumoDespesasPorCategoria(despesas: List[Despesa]) -> Dict[Categoria, float]:
-      pass
+        total_gasto = self.calcularTotalDespesa()
+        progresso = (total_gasto / self._orcamentoMensal) * 100 if self._orcamentoMensal > 0 else 0
+        mensagem_progresso = f"Progresso do orçamento: {progresso:.2f}%"
+        return mensagem_progresso
+
+    def resumoDespesasPorCategoria(self, despesas: List[Despesa]) -> Dict[str, float]:
+
+        resumo: Dict[str, float] = {}
+        for despesa in despesas:
+            categoria = despesa.getCategoria().getNome()
+            if categoria in resumo:
+                resumo[categoria] += despesa.getValor()
+            else:
+                resumo[categoria] = despesa.getValor()
+        return resumo
