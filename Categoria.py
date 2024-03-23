8a59ffc2f3a39aa7bbd15175a9366723ef5153d0
Cecilia Miranda <ceci.miranda254@gmail.com>
2024-03-23 16:05:19 -0300

    [feature] Implementa a lógica dos métodos da classe Categoria com base nos testes realizados no arquivo de teste test_categoria.py

--- Categoria.py
+++ Categoria.py
@@ -1,12 +1,17 @@
+import re
+
 
 class Categoria:
 
-    def __init__(self,nome:str) -> None:
+    def __init__(self, nome: str) -> None:
+        if nome is None or nome.strip() == "" or re.match(r'^\d+$', nome):
+            raise ValueError("O nome da categoria não pode ser vazio ou consistir apenas de números.")
         self._nome = nome
 
-    
-    def getNome(self)->str:
+    def getNome(self) -> str:
         return self._nome
-    
-    def setNome(self,nome:str)->None:
+
+    def setNome(self, nome: str) -> None:
+        if nome is None or nome.strip() == "" or re.match(r'^\d+$', nome):
+            raise ValueError("O nome da categoria não pode ser vazio ou consistir apenas de números.")
         self._nome = nome
